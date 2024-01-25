4621994df2a2c91fb73816a0fd26a41a8111c875
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-25 19:56:50 -0300

    test(test): add layout test for "testing" purpose

---
+++ layouts/default.test.ts
@@ -0,0 +1,11 @@
+import { mount } from "@vue/test-utils";
+import { describe, expect, test } from "vitest";
+import DefaultLayout from "./default.vue";
+
+describe("layouts/default.vue", () => {
+  test("renders correctly a <main> tag", () => {
+    const wrapper = mount(DefaultLayout);
+
+    expect(wrapper.get("main")).toBeTruthy();
+  });
+});
