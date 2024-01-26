8bb352cffe0d7e361aa6febc7e0de984140e5677
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-26 16:25:30 -0300

    feat(navbar): create icon buttons on navbar

---
+++ components/NavBar/ItemButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button>
+    <div class="padding-10 flex align-center">
+      <slot />
+    </div>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+button {
+  background-color: transparent;
+  aspect-ratio: 1/1;
+}
+</style>
---
+++ components/NavBar/Notification.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container flex align-center">
+    <div class="dot"></div>
+    <img src="/images/NavBar/notification-3-line.svg" alt="Notifications" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  position: relative;
+}
+
+.dot {
+  aspect-ratio: 1;
+  width: 4px;
+  height: 4px;
+  background-color: $red-base;
+  border: 2px solid $white-0;
+  border-radius: 50%;
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+</style>
