cde402df2a0d537e18f372b514df29fd237ca7fd
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-26 16:26:00 -0300

    feat(navbar): join all child components in main navbar component

---
+++ components/NavBar/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="padding-x-32 padding-y-20 flex gap-12 space-between">
+    <NavBarUserInfo class="userInfo" />
+    <NavBarItemButton>
+      <img src="/images/NavBar/search-2-line.svg" alt="Notifications" />
+    </NavBarItemButton>
+    <NavBarItemButton>
+      <NavBarNotification />
+    </NavBarItemButton>
+
+    <slot />
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+.userInfo {
+  flex: 1;
+}
+</style>
