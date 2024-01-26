062cc12974ae1dbd833516d72f2200d2d81ab84f
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-26 16:24:55 -0300

    feat(navbar): create UserInfo component on navbarr

---
+++ components/NavBar/UserInfo.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { UserService } from "~/services/UserService";
+
+const _userService = new UserService();
+
+const { name, avatar, gender } = _userService.getLoggedUser();
+</script>
+
+<template>
+  <div class="flex gap-14 align-center">
+    <Avatar :name="name" :src="avatar" :size="48" />
+    <NavBarUserData :name="name" :gender="gender" />
+  </div>
+</template>
