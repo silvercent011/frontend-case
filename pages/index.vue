5878304a9a9a1de469c1bb4e947a884d4fe078a4
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-27 01:21:07 -0300

    feat(dashboard): create a dashboard items for testing

--- pages/index.vue
+++ pages/index.vue
@@ -3,4 +3,45 @@
     <Button icon="ri:calendar-check-line">Calendário</Button>
     <Button variant="primary" icon="ri:add-line">Nova solicitação</Button>
   </NavBar>
+
+  <div class="grid padding-x-32 padding-y-4">
+    <Card title="Rastreador de Tempo" icon="ri:timer-flash-line">
+      <template #header>
+        <Button small icon="ri:history-line">Histórico</Button>
+      </template>
+    </Card>
+
+    <Card title="Horas diárias de trabalho" icon="ri:timer-line">
+      <template #header>
+        <Button small>Detalhes</Button>
+      </template>
+    </Card>
+
+    <Card title="Destaque colaboradores" icon="ri:star-smile-line">
+      <template #header>
+        <Button small icon="ri:share-forward-line">Enviar</Button>
+      </template>
+    </Card>
+
+    <Card title="Cursos - Progresso" icon="ri:book-3-line">
+      <template #header>
+        <Button small>Ver todos</Button>
+      </template>
+    </Card>
+    <Card title="Cursos" icon="ri:book-3-line">
+      <template #header>
+        <input type="text" placeholder="Hello" />
+        <Button small>Ver todos</Button>
+      </template>
+    </Card>
+    <Card title="Feedback diário" icon="ri:discuss-line"> </Card>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 24px;
+}
+</style>
