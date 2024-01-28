8ddad68fd02c5ed2e2124a3bebfe2a5f467b9083
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-28 01:30:50 -0300

    feat(timetracker): create service and import assets

---
+++ services/TimeTrackerActivityService.ts
@@ -0,0 +1,16 @@
+export class TimeTrackerActivityService {
+  public getActivities() {
+    return [
+      {
+        title: "Loom Rebranding",
+        time: "1:23:05",
+        imgSrc: "/images/TimeTracker/Loom.svg",
+      },
+      {
+        title: "Evernote App Redesign",
+        time: "3:14:26",
+        imgSrc: "/images/TimeTracker/Evernote.svg",
+      },
+    ];
+  }
+}
