f99d46f47fe5d66454a782ed01854f98c3dd86f5
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2024-01-28 15:33:39 -0300

    feat(coursesprogress): create service and update styles

---
+++ services/CoursesService.ts
@@ -0,0 +1,11 @@
+export class CoursesService {
+  public getFeaturedCourse() {
+    return {
+      title: "Diversidade da equipe",
+      subtitle:
+        "Projetado para promover a inclusividade e perspectivas diversas.",
+      progress: 25,
+      linkLabel: "Retomar Curso",
+    };
+  }
+}
